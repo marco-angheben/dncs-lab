c95fd597ba519b15b816e6812f862169115f81b9
marco-angheben <68732019+marco-angheben@users.noreply.github.com>
2020-11-28 18:07:31 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -1,4 +1,11 @@
 export DEBIAN_FRONTEND=noninteractive
+
+sudo ip link set dev enp0s8 up
+sudo ip add add 192.168.7.4/24 dev enp0s8
+sudo ip route add 10.1.1.0/30 via 192.168.7.1
+sudo ip route add 192.168.0.0/21 via 192.168.7.1
+
+
 sudo apt-get update                                     #update and upgrade of applications
 sudo apt-get upgrade
 sudo apt-get install -y docker.io                       #installation of docker engine
@@ -8,8 +15,4 @@ sudo docker run --name web-server -p 80:80 -d nginx     #running docker called '
 
 # Startup commands go here
 
-sudo ip link set dev enp0s8 up
-sudo ip add add 192.168.7.4/24 dev enp0s8
-sudo ip route add 10.1.1.0/30 via 192.168.7.1
-sudo ip route add 192.168.0.0/21 via 192.168.7.1
 
