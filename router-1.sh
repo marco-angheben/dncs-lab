16c7d3d80e8fe5b14b3593691adbbeb8edef665e
marco-angheben <68732019+marco-angheben@users.noreply.github.com>
2020-11-28 11:20:07 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -9,6 +9,6 @@ sudo ip link set dev enp0s9 up
 sudo ip link set dev enp0s8 up
 sudo ip link add link enp0s8 name enp0s8.2 type vlan id 2
 sudo ip link add link enp0s8 name enp0s8.3 type vlan id 3
-sudo ip add add 192.168.4.1/23 dev enp0s8.2
+sudo ip add add 192.168.0.1/23 dev enp0s8.2
 sudo ip add add 192.168.3.1/25 dev enp0s8.3
 sudo ip route add 192.168.7.0/24 via 10.1.1.2
