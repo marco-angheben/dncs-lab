b7ab68d7da0b88b6df8a8ca50ca379fefcaab58c
marco-angheben <68732019+marco-angheben@users.noreply.github.com>
2020-11-28 10:45:57 +0100

    Update host-1-b.sh

--- host-1-b.sh
+++ host-1-b.sh
@@ -1,1 +1,8 @@
+export DEBIAN_FRONTEND=noninteractive
 
+# Startup commands go here
+
+sudo ip link set dev enp0s8 up
+sudo ip add add 192.168.3.4/25 dev enp0s8
+sudo ip route add 10.1.1.0/30 via 192.168.3.1
+sudo ip route add 192.168.0.0/21 via 192.168.3.1
