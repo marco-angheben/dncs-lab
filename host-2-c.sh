a0155eddaa7a0a310955fa8a2e5810400937af60
marco-angheben <68732019+marco-angheben@users.noreply.github.com>
2020-11-28 10:55:53 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -1,1 +1,16 @@
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt -y install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo docker pull dustnic82/nginx-test
+sudo docker run --name nginx -p 80:80 -d dustnic82/nginx-test
+
+# Startup commands go here
+
+sudo ip link set dev enp0s8 up
+sudo ip add add 192.168.7.4/24 dev enp0s8
+sudo ip route add 10.1.1.0/30 via 192.168.7.1
+sudo ip route add 192.168.0.0/21 via 192.168.7.1
 
