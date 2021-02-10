7c6b2a704ad7c590a3b67132ba43d60c6e911510
marco-angheben <68732019+marco-angheben@users.noreply.github.com>
2021-02-10 15:41:06 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -3,12 +3,19 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Startup commands go here
 
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo ip add add 10.1.1.1/30 dev enp0s9
-sudo ip link set dev enp0s9 up
-sudo ip link set dev enp0s8 up
 sudo ip link add link enp0s8 name enp0s8.2 type vlan id 2
 sudo ip link add link enp0s8 name enp0s8.3 type vlan id 3
+
+sudo ip link set dev enp0s9 up
+sudo ip link set dev enp0s8 up
+sudo ip link set dev enp0s8.2 up
+sudo ip link set dev enp0s8.3 up
+
+
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo ip add add 10.1.1.1/30 dev enp0s9
 sudo ip add add 192.168.0.1/23 dev enp0s8.2
 sudo ip add add 192.168.3.1/25 dev enp0s8.3
+
 sudo ip route add 192.168.7.0/24 via 10.1.1.2
+
