6326f4dd53bf729375fff2b6c210fa529f629586
marco-angheben <68732019+marco-angheben@users.noreply.github.com>
2021-02-10 21:29:23 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -5,14 +5,14 @@ sudo ip add add 192.168.7.4/24 dev enp0s8
 sudo ip route add 10.1.1.0/30 via 192.168.7.1
 sudo ip route add 192.168.0.0/21 via 192.168.7.1
 
+#install docker
+apt-get update
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
-sudo apt-get update                                     #update and upgrade of applications
-sudo apt-get upgrade
-sudo apt-get install -y docker.io                       #installation of docker engine
-sudo docker pull nginx                                  #downloading nginx image
-sudo docker run --name web-server -p 80:80 -d nginx     #running docker called 'web-server' using nginx on port 80:80
-
-
-# Startup commands go here
-
-
+# Pulling and running the nginx image
+docker pull dustnic82/nginx-test
+docker run -d -p 80:80 dustnic82/nginx-test
